3475f7efcac355099d08bc1043a17afa63c17437
falleng0d <juniormateusknd@gmail.com>
2021-08-13 09:05:40 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,ts,html}"],
+  theme: {
+    extend: {
+      colors: {
+        sky: "#22d3ee",
+        cyan: "#22d3ee"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
